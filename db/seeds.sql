c4443b7f3947d1a1cc5ce8380c9ee38c97474df2
DutchMon <dutch.holland13@gmail.com>
2018-08-08 16:17:47 -0700

    finished hw

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('Bacon Burger');
+INSERT INTO burgers (name) VALUES ('Diablo Burger');
+INSERT INTO burgers (name, devoured) VALUES ('Mushroom Burger', true);
+INSERT INTO burgers (name, devoured) VALUES ('Onion Ring Burger', true);
+INSERT INTO burgers (name, devoured) VALUES ('BBQ Burger', true);
+INSERT INTO burgers (name) VALUES ('Chili Burger');
